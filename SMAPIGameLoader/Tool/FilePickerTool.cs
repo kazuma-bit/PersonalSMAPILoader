e42a692cd6a3fff2b5d70603aca8d2ae94341cbc
kazuma-bit <78422998+kazuma-bit@users.noreply.github.com>
2026-07-02 17:06:05 +0800

    Update FilePickerTool.cs

--- SMAPIGameLoader/Tool/FilePickerTool.cs
+++ SMAPIGameLoader/Tool/FilePickerTool.cs
@@ -1,4 +1,4 @@
-﻿using Android;
+using Android;
 using Android.Content.PM;
 using Android.OS;
 
@@ -6,6 +6,7 @@ using AndroidX.Core.App;
 using AndroidX.Core.Content;
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Xamarin.Essentials;
@@ -18,13 +19,9 @@ internal static class FilePickerTool
     {
         { DevicePlatform.Android, new[] { "application/zip" } },
     });
-    public static async Task<FileResult> PickZipFile(string title = null)
+
+    static bool HasStoragePermission()
     {
-        if (title == null)
-            title = "Please select zip file";
-        
-        //TODO check permission
-        //android 6 --> 10 need to request External read write storage
         var activity = Launcher.LauncherActivity.Instance;
         if (Build.VERSION.SdkInt >= BuildVersionCodes.M && Build.VERSION.SdkInt <= BuildVersionCodes.Q)
         {
@@ -32,13 +29,22 @@ internal static class FilePickerTool
                 || ContextCompat.CheckSelfPermission(activity, Manifest.Permission.WriteExternalStorage) != Permission.Granted)
             {
                 ToastNotifyTool.Notify("Please Click Allow File Access Permission");
-                // Request the permissions
                 ActivityCompat.RequestPermissions(activity,
                     [Manifest.Permission.ReadExternalStorage, Manifest.Permission.WriteExternalStorage],
-                    1000); // 100 is a request code (you can choose any integer)
-                return null;
+                    1000);
+                return false;
             }
         }
+        return true;
+    }
+
+    public static async Task<FileResult> PickZipFile(string title = null)
+    {
+        if (title == null)
+            title = "Please select zip file";
+
+        if (!HasStoragePermission())
+            return null;
 
         var options = new PickOptions
         {
@@ -46,5 +52,22 @@ internal static class FilePickerTool
             FileTypes = FileTypeZip,
         };
         return await FilePicker.PickAsync(options);
+    }
+
+    public static async Task<List<FileResult>> PickMultipleZipFiles(string title = null)
+    {
+        if (title == null)
+            title = "Please select zip file(s)";
+
+        if (!HasStoragePermission())
+            return null;
+
+        var options = new PickOptions
+        {
+            PickerTitle = title,
+            FileTypes = FileTypeZip,
+        };
+        var results = await FilePicker.PickMultipleAsync(options);
+        return results?.ToList();
     }
 }
